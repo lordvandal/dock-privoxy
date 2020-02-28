eab763c41a6f3745c947fda67a852a808b0d1d15
attilabedo <atiofdnt@yahoo.com>
2020-02-28 16:52:10 +0200

    Update and rename run.sh.new to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-#bash syntax with [[ and ]], Bourne shell (ash/busybox) syntax for alpine linux is with [ and ]
+#!/bin/ash
 
 PRIVOXYDIR=/etc/privoxy
 EASYLISTA=easylist.script.filter
@@ -12,7 +10,7 @@ EASYLISTB=easylist.script.action
 PRIVOXY_CONF=/etc/privoxy/config
 FORWARD_RULE="forward   /       "
 RET=$(grep "^${FORWARD_RULE}" ${PRIVOXY_CONF})
-if [ "$FORWARD_DNS" != "" ] && [ "$FORWARD_PORT" != "" ]; then
+if [[ ("$FORWARD_DNS" != "") && ("FORWARD_PORT" != "") ]]; then
   echo "Forwarding DNS exist. Adding forwarding directive to ${PRIVOXY_CONF}"
   #comment out existing directive
   sed -i "/$(echo "${FORWARD_RULE}" | sed "s#\/#\\\/#g")/d" ${PRIVOXY_CONF}
@@ -20,11 +18,11 @@ if [ "$FORWARD_DNS" != "" ] && [ "$FORWARD_PORT" != "" ]; then
   echo "${FORWARD_RULE}${FORWARD_DNS}:${FORWARD_PORT}" >> ${PRIVOXY_CONF}
 fi
 
-if [ -e "$PRIVOXYDIR/$EASYLISTA" ] && [ -e "$PRIVOXYDIR/$EASYLISTB" ]; then
+if [[ (-e "$PRIVOXYDIR/$EASYLISTA") && (-e "$PRIVOXYDIR/$EASYLISTB") ]]; then
   echo "Easy Lists Found. Skipping download..."
 else
   echo "Files not found! Running download..."
-  . /usr/local/bin/privoxy-blist.sh -v 2
+  /usr/local/bin/privoxy-blist.sh
 fi
 
 /usr/sbin/privoxy --no-daemon /etc/privoxy/config
